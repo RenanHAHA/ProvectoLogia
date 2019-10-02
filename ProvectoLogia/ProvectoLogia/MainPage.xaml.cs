74b436bfcfa99de985664e25f518c44298cce654
renan cruz <renanzinho999@hotmail.com>
2019-10-02 14:54:53 -0300

    Feito O menu de Opções

--- ProvectoLogia/ProvectoLogia/MainPage.xaml.cs
+++ ProvectoLogia/ProvectoLogia/MainPage.xaml.cs
@@ -13,5 +13,34 @@ namespace ProvectoLogia
         {
             InitializeComponent();
         }
+
+        private void ButtonEntrar_Clicked(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(entryUsuario.Text) ||
+             !string.IsNullOrEmpty(entrySenha.Text))
+            {
+
+                if ((entryUsuario.Text == "Admin") || (entrySenha.Text == "123"))
+                {
+                    Navigation.PushAsync(new MenuOP());
+                }
+                else
+                {
+                    DisplayAlert("Erro", "Não foi possivel entrar! \nErro no nome de Login ou Senha", "Ok");
+                }
+
+            }
+            else
+            {
+                DisplayAlert("ERRO", "Não deixe os campos em branco", "OK");
+            }
+
+        }
+
+        private void ButtonCadastrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CadastroDeUsuario());
+        }
     }
 }
---
+++ ProvectoLogia/ProvectoLogia/MenuOP.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProvectoLogia
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuOP : MasterDetailPage
+    {
+        public MenuOP()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new Menu());
+        }
+
+        private void PaginaSobre_Tapped(object sender, EventArgs e)
+        {
+            // Criei errado ;---;
+        }
+
+        private void PaginaSobre_Tapped_1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Sobre());
+            IsPresented = false;//Fecha o menu
+        }
+
+        private void PaginaDeLeitor_Tapped(object sender, EventArgs e)
+        {
+            //Detail = new NavigationPage(new LeitorQR());
+            //IsPresented = false;
+        }
+
+        private void PaginaContato_Tapped(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Contato());
+            IsPresented = false;
+        }
+
+        private void PaginaGaleria_Tapped(object sender, EventArgs e)
+        {
+            //Detail = new NavigationPage(new Galeria());
+            //IsPresented = false;
+            //Esta em procedimento
+            //Ainda não esta feito
+        }
+
+        private void PaginaAgendamento_Tapped(object sender, EventArgs e)
+        {
+            //Detail = new NavigationPage(new Agendamento());
+            //IsPresented = false;
+            //Esta em procedimento
+            //Ainda não esta feito
+        }
+    }
+}
